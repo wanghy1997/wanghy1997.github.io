4913100e76e4ec9e0bf58e80e7a2ef1edf8c314b
wanghy1997 <wendong.6267@gmail.com>
2025-09-26 06:22:34 +0000

    Deploying to gh-pages from @ wanghy1997/wanghy1997.github.io@44c8e074a2fbb05d9411def89d7c98e0c4a1cd73 🚀

---
+++ assets/apps.html-CG9mAtMv.js
@@ -0,0 +1,47 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as o,a as n,o as r}from"./app-DPphJjrU.js";const a={};function i(p,t){return r(),o("div",null,[...t[0]||(t[0]=[n("pre",null,[n("code",null,`1.	海豚湾
+2.	office
+3.	vscode
+4.	微信/QQ
+5.	Nikon NX Studio
+6.	Lightroom
+7.	Topaz Photo AI
+8.	Photoshop / Illustrator /pr/me
+9.	adobe acrobat pro dc
+10.	The Unarchiver
+11.	chrome/edge
+12.	百度网盘/阿里网盘/夸克网盘
+13.	PP Duck 
+14.	FileZilla
+15.	Downie / Permute 3
+16.	Movist Pro
+17.	iStat Menu
+18.	typora/pandoc
+19.	termius
+20.	Easy Connect（学校VPN）
+21.	omnigraffle
+22.	DiffusionBee
+23.	Luminar Neo
+24.	Resolve Studio（达芬奇）
+25.	homebrew
+26.	Free Download Manager 
+27.	Server Cat
+28.	GraphicConverter
+29.	Maccy 0.29.4 剪贴板管理
+30.	腾讯会议
+31.	新建文件++
+32.	Amphetamine
+33.	HandBrake
+34.	向日葵（SunLoginClient）
+35.	Miniconda
+36.	JetBrainsMono（字体）
+37.	Quick Look plugins (github 预览插件)
+38.	bears countdown
+39.	xmind pro
+40.	RightFont
+41.	LRTimelapse / Adobe DNG Converter
+42.	imagenomic portraiture 4 for lr
+43.	Gapplin（SVG浏览）
+44.	腾讯青柠
+45.	飞书
+46.	OmniPlan Pro
+`)],-1)])])}const l=e(a,[["render",i]]),s=JSON.parse(`{"path":"/coooder/mac/apps.html","title":"","lang":"en-US","frontmatter":{"head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2025-09-17T15:06:21.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Hongyi Wang\\",\\"url\\":\\"https://wanghy1997.github.io/wanghy1997\\"}]}"],["meta",{"property":"og:url","content":"https://wanghy1997.github.io/wanghy1997/coooder/mac/apps.html"}],["meta",{"property":"og:site_name","content":"Hongyi's Blog"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2025-09-17T15:06:21.000Z"}],["meta",{"property":"article:modified_time","content":"2025-09-17T15:06:21.000Z"}]]},"git":{"createdTime":1758121581000,"updatedTime":1758121581000,"contributors":[{"name":"whymbp","username":"whymbp","email":"why_6267@163.com","commits":1,"url":"https://github.com/whymbp"}]},"readingTime":{"minutes":0.6,"words":180},"filePathRelative":"coooder/mac/apps.md","excerpt":"<pre><code>1.\\t海豚湾\\n2.\\toffice\\n3.\\tvscode\\n4.\\t微信/QQ\\n5.\\tNikon NX Studio\\n6.\\tLightroom\\n7.\\tTopaz Photo AI\\n8.\\tPhotoshop / Illustrator /pr/me\\n9.\\tadobe acrobat pro dc\\n10.\\tThe Unarchiver\\n11.\\tchrome/edge\\n12.\\t百度网盘/阿里网盘/夸克网盘\\n13.\\tPP Duck \\n14.\\tFileZilla\\n15.\\tDownie / Permute 3\\n16.\\tMovist Pro\\n17.\\tiStat Menu\\n18.\\ttypora/pandoc\\n19.\\ttermius\\n20.\\tEasy Connect（学校VPN）\\n21.\\tomnigraffle\\n22.\\tDiffusionBee\\n23.\\tLuminar Neo\\n24.\\tResolve Studio（达芬奇）\\n25.\\thomebrew\\n26.\\tFree Download Manager \\n27.\\tServer Cat\\n28.\\tGraphicConverter\\n29.\\tMaccy 0.29.4 剪贴板管理\\n30.\\t腾讯会议\\n31.\\t新建文件++\\n32.\\tAmphetamine\\n33.\\tHandBrake\\n34.\\t向日葵（SunLoginClient）\\n35.\\tMiniconda\\n36.\\tJetBrainsMono（字体）\\n37.\\tQuick Look plugins (github 预览插件)\\n38.\\tbears countdown\\n39.\\txmind pro\\n40.\\tRightFont\\n41.\\tLRTimelapse / Adobe DNG Converter\\n42.\\timagenomic portraiture 4 for lr\\n43.\\tGapplin（SVG浏览）\\n44.\\t腾讯青柠\\n45.\\t飞书\\n46.\\tOmniPlan Pro\\n</code></pre>"}`);export{l as comp,s as data};
